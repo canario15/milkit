e52bb205c180c06b54ef61d5da711885f39bcb7e
German Fagalde <germanfagalde@gmail.com>
2019-09-20 20:53:20 -0300

    add password in active admin new user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,9 +1,11 @@
 ActiveAdmin.register User do
-  permit_params %i[email]
+  permit_params %i[email password password_confirmation]
 
   form do |f|
-    f.inputs 'User' do
+    f.inputs "Usuarios" do
       f.input :email
+      f.input :password
+      f.input :password_confirmation
     end
     f.actions
   end
