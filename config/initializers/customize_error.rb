4cc5ddb8556e9ef0bb8c4979e84442884d83cadd
German Fagalde <germanfagalde@gmail.com>
2019-11-20 22:03:50 -0300

    fix calendar errors show

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -14,9 +14,9 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
       html = %(<div class="control-group error-field">#{e}</div>).html_safe
     elsif form_fields.include? e.node_name
       if instance.error_message.kind_of?(Array)
-        html = %(<div class="control-group error-field">#{html_tag}<span class="help-inline">&nbsp;#{instance.error_message.uniq.join(', ')}</span></div>).html_safe
+        html = %(<div class="error-field">#{html_tag}<div class="clearfix"></div><span class="help-inline">&nbsp;#{instance.error_message.uniq.join(', ')}</span></div>).html_safe
       else
-        html = %(<div class="control-group error-field">#{html_tag}<span class="help-inline">&nbsp;#{instance.error_message}</span></div>).html_safe
+        html = %(<div class="error-field">#{html_tag}<div class="clearfix"></div><span class="help-inline">&nbsp;#{instance.error_message}</span></div>).html_safe
       end
     end
   end
